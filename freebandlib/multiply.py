ce55152c1256793e94ab7754fb54c08a10e2a62a
reiniscirpons <rc234@st-andrews.ac.uk>
2022-04-07 15:04:45 +0100

    Propagate changes to multiplication. Fix all tests.

--- freebandlib/multiply.py
+++ freebandlib/multiply.py
@@ -34,10 +34,10 @@ def compute_k(
         The partial function :math:`\overline{K}_{\alpha}` implemented as a
         two dimensional list.
     """
-    q_x = transducer_precompute_q(transducer_x, 1)
-    q_y = transducer_precompute_q(transducer_y, 0)
-    cont_x = transducer_cont(transducer_x)
-    cont_y = transducer_cont(transducer_y)
+    q_x = transducer_precompute_q(transducer_x.initial, 1, transducer_x)
+    q_y = transducer_precompute_q(transducer_y.initial, 0, transducer_y)
+    cont_x = transducer_cont(transducer_x.initial, transducer_x)
+    cont_y = transducer_cont(transducer_y.initial, transducer_y)
     K: List[List[Optional[int]]] = [
         [None for j in range(len(cont_y) + 1)] for i in range(len(cont_x) + 1)
     ]
@@ -45,8 +45,7 @@ def compute_k(
     if alpha == 0:
         for i in range(len(cont_x), -1, -1):
             for j in range(len(cont_y), -1, -1):
-                state = transducer_y.states[q_y[j]]
-                letter = state.next_letter[0]
+                letter = transducer_y.next_letter[q_y[j]][0]
                 if letter is not None and letter not in c:
                     K[i][j] = 1
                 elif (
@@ -56,16 +55,14 @@ def compute_k(
                 ):
                     K[i][j] = 1 + K[i][j + 1]
             if i != 0:
-                state = transducer_x.states[q_x[i - 1]]
-                letter = state.next_letter[1]
+                letter = transducer_x.next_letter[q_x[i - 1]][1]
                 c.add(letter)
         return K
     # TODO: Remove code duplication
     # alpha == 1
     for j in range(len(cont_y), -1, -1):
         for i in range(len(cont_x), -1, -1):
-            state = transducer_x.states[q_x[i]]
-            letter = state.next_letter[1]
+            letter = transducer_x.next_letter[q_x[i]][1]
             if letter is not None and letter not in c:
                 K[i][j] = 1
             elif (
@@ -75,53 +72,57 @@ def compute_k(
             ):
                 K[i][j] = 1 + K[i + 1][j]
         if j != 0:
-            state = transducer_y.states[q_y[j - 1]]
-            letter = state.next_letter[0]
+            letter = transducer_y.next_letter[q_y[j - 1]][0]
             c.add(letter)
     return K
 
 
 def multiply(transducer_x: Transducer, transducer_y: Transducer) -> Transducer:
-    product_transducer = Transducer(None, [], [])
+    product_transducer = Transducer(None, [], [], [])
     # Copy each of the existing transducers
     inclusion_x: List[Optional[StateId]] = [
-        None for state in transducer_x.states
+        None for state in range(transducer_x.nr_states)
     ]
     inclusion_y: List[Optional[StateId]] = [
-        None for state in transducer_y.states
+        None for state in range(transducer_y.nr_states)
     ]
     for transducer, inclusion in [
         (transducer_x, inclusion_x),
         (transducer_y, inclusion_y),
     ]:
-        for state in transducer.states:
-            new_state = product_transducer.add_state(
-                [None, None], [None, None], transducer.terminal[state.state_id]
+        for state in range(transducer.nr_states):
+            inclusion[state] = product_transducer.add_state(
+                [None, None], [None, None], transducer.terminal[state]
             )
-            inclusion[state.state_id] = new_state.state_id
 
     for transducer, inclusion in [
         (transducer_x, inclusion_x),
         (transducer_y, inclusion_y),
     ]:
-        for state in transducer.states:
-            inclusion_id = inclusion[state.state_id]
-            assert inclusion_id is not None
-            product_state = product_transducer.states[inclusion_id]
+        for state in range(transducer.nr_states):
+            product_state = inclusion[state]
+            assert product_state is not None
             for letter in [0, 1]:
-                product_state.next_letter[letter] = state.next_letter[letter]
-                child = state.next_state[letter]
+                product_transducer.next_letter[product_state][
+                    letter
+                ] = transducer.next_letter[state][letter]
+                child = transducer.next_state[state][letter]
                 if child is not None:
-                    inclusion_child_id = inclusion[child.state_id]
-                    assert inclusion_child_id is not None
-                    product_state.next_state[
+                    assert inclusion[child] is not None
+                    product_transducer.next_state[product_state][
                         letter
-                    ] = product_transducer.states[inclusion_child_id]
+                    ] = inclusion[child]
 
-    q_x = transducer_precompute_q(transducer_x, 1)
-    q_y = transducer_precompute_q(transducer_y, 0)
-    cont_x = transducer_cont(transducer_x)
-    cont_y = transducer_cont(transducer_y)
+    assert inclusion_x[transducer_x.initial] is not None
+    assert inclusion_y[transducer_y.initial] is not None
+    q_x = transducer_precompute_q(
+        inclusion_x[transducer_x.initial], 1, product_transducer
+    )
+    q_y = transducer_precompute_q(
+        inclusion_y[transducer_y.initial], 0, product_transducer
+    )
+    cont_x = transducer_cont(transducer_x.initial, transducer_x)
+    cont_y = transducer_cont(transducer_y.initial, transducer_y)
     K0 = compute_k(0, transducer_x, transducer_y)
     K1 = compute_k(1, transducer_x, transducer_y)
 
@@ -129,59 +130,50 @@ def multiply(transducer_x: Transducer, transducer_y: Transducer) -> Transducer:
         [None for j in range(len(cont_y) + 1)] for i in range(len(cont_x) + 1)
     ]
     reverse_state_lookup: List[Optional[Tuple[int, int]]] = [
-        None for state in product_transducer.states
+        None for state in range(product_transducer.nr_states)
     ]
     for i in range(len(cont_x), -1, -1):
         for j in range(len(cont_y), -1, -1):
-            next_state_id: List[Optional[StateId]] = [None, None]
+            next_state: List[Optional[StateId]] = [None, None]
             next_letter: List[Optional[OutputLetter]] = [None, None]
             if K0[i][j] is not None:
-                next_state_id[0] = state_lookup[i][j + K0[i][j]]
-                next_letter[0] = product_transducer.states[
-                    inclusion_y[q_y[j + K0[i][j] - 1]]
-                ].next_letter[0]
+                next_state[0] = state_lookup[i][j + K0[i][j]]
+                next_letter[0] = product_transducer.next_letter[
+                    q_y[j + K0[i][j] - 1]
+                ][0]
             else:
-                inclusion_state = product_transducer.states[inclusion_x[q_x[i]]]
                 if (
-                    inclusion_state is not None
-                    and inclusion_state.next_state[0] is not None
+                    q_x[i] is not None
+                    and product_transducer.next_state[q_x[i]][0] is not None
                 ):
-                    next_state_id[0] = inclusion_state.next_state[0].state_id
-                    next_letter[0] = inclusion_state.next_letter[0]
+                    next_state[0] = product_transducer.next_state[q_x[i]][0]
+                    next_letter[0] = product_transducer.next_letter[q_x[i]][0]
             if K1[i][j] is not None:
-                next_state_id[1] = state_lookup[i + K1[i][j]][j]
-                next_letter[1] = product_transducer.states[
-                    inclusion_x[q_x[i + K1[i][j] - 1]]
-                ].next_letter[1]
+                next_state[1] = state_lookup[i + K1[i][j]][j]
+                next_letter[1] = product_transducer.next_letter[
+                    q_x[i + K1[i][j] - 1]
+                ][1]
             else:
-                inclusion_state = product_transducer.states[inclusion_y[q_y[j]]]
                 if (
-                    inclusion_state is not None
-                    and inclusion_state.next_state[1] is not None
+                    q_y[j] is not None
+                    and product_transducer.next_state[q_y[j]][1] is not None
                 ):
-                    next_state_id[1] = inclusion_state.next_state[1].state_id
-                    next_letter[1] = inclusion_state.next_letter[1]
+                    next_state[1] = product_transducer.next_state[q_y[j]][1]
+                    next_letter[1] = product_transducer.next_letter[q_y[j]][1]
             state_lookup[i][j] = product_transducer.add_state(
-                next_state_id, next_letter, False
-            ).state_id
+                next_state, next_letter, False
+            )
             reverse_state_lookup.append((i, j))
 
     product_transducer.initial = state_lookup[0][0]
-    for state in product_transducer.states:
+    for state in range(product_transducer.nr_states):
         for letter in [0, 1]:
-            if (
-                state.next_state[letter] is not None
-                and reverse_state_lookup[state.next_state[letter].state_id]
-                is not None
-            ):
-                i, j = reverse_state_lookup[state.next_state[letter].state_id]
+            child = product_transducer.next_state[state][letter]
+            if child is not None and reverse_state_lookup[child] is not None:
+                i, j = reverse_state_lookup[child]
                 if j == len(cont_y):
-                    state.next_state[letter] = product_transducer.states[
-                        inclusion_x[q_x[i]]
-                    ]
+                    product_transducer.next_state[state][letter] = q_x[i]
                 elif i == len(cont_x):
-                    state.next_state[letter] = product_transducer.states[
-                        inclusion_y[q_y[j]]
-                    ]
+                    product_transducer.next_state[state][letter] = q_y[j]
 
     return product_transducer
