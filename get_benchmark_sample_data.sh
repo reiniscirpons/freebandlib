474b1ff8f189ff2bdc3a12104e26045f120eb782
reiniscirpons <rc234@st-andrews.ac.uk>
2022-08-30 00:34:52 +0100

    Fix bug in sample data download script

--- get_benchmark_sample_data.sh
+++ get_benchmark_sample_data.sh
@@ -2,7 +2,7 @@
 BASE_URL="https://github.com/reiniscirpons/freebandlib/releases/download"
 TAG="v0.0.1"
 OUT_DIR="./benchmarks/samples"
-mkdir -p OUT_DIR
+mkdir -p $OUT_DIR
 
 PREFIX="interval_transducer_"
 curl -L "$BASE_URL/$TAG/$PREFIX[00-19].gz" -o "$OUT_DIR/$PREFIX#1.gz"
